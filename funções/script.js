8b9950cd6201ff95b971539c7b39653b1b95bd6f
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2025-03-27 12:57:50 -0300

    iNIT 2

---
+++ funções/script.js
@@ -0,0 +1,30 @@
+function moveon() {
+  var answer = confirm("Ready to move on?");
+  if (answer) window.location = "http://google.com";
+}
+setTimeout(moveon, 60000);
+
+function debug(msg) {
+  var log = document.getElementById("div");
+  if (!log) {
+    log.id = "debuglog";
+
+    log.innerHTML = "<h1> Debug Log</h1>";
+    document.body.appendChild(log);
+  }
+  var pre = document.createElement("pre");
+  var text = document.createTextNode(msg);
+}
+
+function hide(e, reflow) {
+  if (reflow) {
+    e.style.display = "none";
+  } else {
+    e.style.visibility = "hidden";
+  }
+}
+
+function highlight(e) {
+  if (!e.className) e.className = "#hilite";
+  else e.className += "#hilite";
+}
